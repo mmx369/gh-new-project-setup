fe5210b3317a18ddd38f2f10114cac35dea21294
mmx369 <maksim@klishevich.com>
2022-12-01 18:38:06 +0300

    ffttee

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <header>
-        <h1>GitHub Actions! - another brunch</h1>
+        <h1>GitHub Actions! - XXX</h1>
       </header>
     </div>
   )
